217589ae6fecbcbce90dc9a3163fe51c873d8573
Dmitry Verteyko <verteyko1990@gmail.com>
2022-08-01 13:03:08 +0300

    Initial commit

---
+++ helloVue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Login app</h1>
+    <input type="text" v-model="nameValue">
+    My name is {{ nameValue }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue!",
+      nameValue: ""
+    };
+  },
+};
+</script>
